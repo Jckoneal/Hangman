1794e33601a712f2a01df5cc49485068bc12c728
Jckoneal <44105430+Jckoneal@users.noreply.github.com>
2024-02-15 15:19:39 -0800

    Update stickman.py

--- stickman.py
+++ stickman.py
@@ -1,14 +1,24 @@
+from wonderwords import RandomWord
 from wonderwords import RandomSentence
 import sys
 wrongGuesses = 0
 guesses = []
 
-def randomPhrase():
-  rs = RandomSentence()
-  return rs.sentence()
+def randomPhrase(): #Creates a Random Sentence using the Wonderwords library
+    global phrase
+    WorP = input('Would you like to play with a word (hard) of phrase (easy): ')
+    if WorP.lower().rstrip() == 'phrase' or WorP.lower().rstrip() == 'easy':
+        rs = RandomSentence()
+        phrase = rs.sentence()
+    elif WorP.lower().rstrip() == 'word' or WorP.lower().rstrip() == 'hard':
+        rs = RandomWord()
+        phrase = rs.word()
+    else:
+        print('Please input "word" or "phrase"')
+        randomPhrase()
+    return phrase
 
-
-def generateSpaces(sentence):
+def generateSpaces(sentence): # Makes a list of underscores that is used as the displayed gameboard that gets filled with guesses
     global allSpaces
     global letters
     global wordsInSentence
@@ -17,7 +27,6 @@ def generateSpaces(sentence):
     sentenceNoPeriod = sentence.rstrip('.')
     wordsInSentence = sentenceNoPeriod.split()
     allSpaces = []
-
 
     for word in wordsInSentence:
         letters = list(word)
@@ -28,7 +37,7 @@ def generateSpaces(sentence):
         for l in range(len(spaces)):
             allSpaces.append(spaces[l])
 
-def printSpaces():
+def printSpaces(): #Prints the gameboard
     x = 0
     for i in range(len(wordsInSentence)):
         for d in range(len(wordsInSentence[i])):
@@ -37,15 +46,25 @@ def printSpaces():
         print(' ',end='')
     print('\n')
 
-def checkLetter():
+def inputLetter(): #takes the input and checs it is a single letter
+    global guess
+    guess = input("Guess a letter: ").lower()
+    if guess.isnumeric():
+        print("guess a letter not a number.")
+        inputLetter()
+    elif len(guess) != 1:
+        print('guess 1 letter at a time.')
+        inputLetter()
+    elif guess in guesses:
+        print('You inputed a letter you have already guessed, try again.')
+        inputLetter() 
+    guesses.append(guess)
+
+def checkLetter(): # Checks if the letter is in the random sentence and adds it to the gameboard if so.
     global wrongGuesses
     global guesses
     isTrue = False
-    guess = input("Guess a letter: ").lower()
-    if guess in guesses:
-        print('You inputed a letter you have already guessed, try again.')
-        checkLetter()
-    guesses.append(guess)
+    inputLetter()
     for i in range(len(allLetters)):
         if guess == allLetters[i].lower() and i == 0:
             allSpaces[i] = guess.upper()
@@ -56,7 +75,7 @@ def checkLetter():
     if not isTrue:
         wrongGuesses += 1
 
-def printStickman(wrongGuesses):
+def printStickman(wrongGuesses): # Prints the stickman depending on how many guesses the player had gotten wrong and ends the game when they have no more guesses.
     stickmanParts = [
         ["  _______ ", " |       |", " |", " |", " |", " |", "_|_"], #0 wrong guesses
         ["  _______ ", " |       |", " |       O", " |", " |", " |", "_|_"],  # 1 wrong guesses
@@ -67,25 +86,27 @@ def printStickman(wrongGuesses):
         ["  _______ ", " |       |", " |       O", " |      /|\\", " |      / \\", " |", "_|_"]   # 6 wrong guesses
     ]
 
-
     if wrongGuesses < 6:
         print('\n'.join(stickmanParts[wrongGuesses]))
     else:
         print('\n'.join(stickmanParts[wrongGuesses]))
+        print('The word was:', phrase)
         print("you lost")
         sys.exit()
         
-def hasWon():
+def hasWon(): #checks if the player has won yet
     win = True
     for i in allSpaces:
         if i == '_':
             win = False
     if win:
         return True
+#GameLoop
 generateSpaces(randomPhrase())
 while not hasWon():
     printStickman(wrongGuesses)
     printSpaces()
     checkLetter()
+print('\n')
 printSpaces()
 print('You won!')
